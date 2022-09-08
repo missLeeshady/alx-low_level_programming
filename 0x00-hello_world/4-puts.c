814f7426a7d02b74006effec339679d1be7e55b9
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 16:38:05 -0700

    correction2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void) /* void this specifies there are no arguments taken by main */
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
