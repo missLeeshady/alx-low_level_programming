3ca30f1333409c757518aac40c62f3312b60b25b
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 16:50:44 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Returns: Always 0 (Success)
  */
-int main(void)/* void this specifies there are no arguments taken by main */
+int main()/* void this specifies there are no arguments taken by main */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
