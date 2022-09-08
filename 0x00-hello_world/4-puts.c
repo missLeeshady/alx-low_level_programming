a70111eaa91ae388960d96e30a884771b0aebbe9
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 16:34:45 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Returns: Always 0 (Success)
  */
-int main(void)
+int main(void) /* void this specifies there are no arguments taken by main */
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}	
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void) {/* this specifies there are no arguments taken by main*/}
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return 0;
+}
