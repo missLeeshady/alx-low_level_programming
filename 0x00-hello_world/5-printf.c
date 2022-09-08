4cd38268fc55a6386bc63e32c96f4ea92c969380
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 16:42:17 -0700

    use printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Returns: Always 0 (Success)
  */
-int main(void) {/* this specifies there are no arguments taken by main*/}
+int main(void) /* this specifies there are no arguments taken by main */
 {
 	printf("with proper grammar, but the outcome is a piece of art,/n");
-	return 0;
+	return (0);
 }
