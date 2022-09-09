be154dbe6132c9c33c67958abc382b0b381c6c51
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-09 04:08:12 -0700

    okay

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
  * Returns: Always 0 (Success)
  */
 int main(void)
-/**
- *return value of main is zero
- *
- *description - zero errors were returned
- *
- *no arguments taken by main
- */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
