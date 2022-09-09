cb99faf99b3056d9e473f1a2edaff44a14954d2f
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 17:06:58 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,12 @@
  *
  * Returns: Always 0 (Success)
  */
-int main(void)/* description of main return value; the return value is zero hence, void */
+int main(void)
+/**
+ * description of 'main (void)'
+ *
+ *  the return value is zero hence, void 
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
