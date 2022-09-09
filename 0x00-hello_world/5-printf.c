56025925f2d110f1d012139acf6ac88813caf7e7
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 18:09:25 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -13,6 +13,6 @@ int main(void)
  * and there are no arguments taken by main 
  */
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
