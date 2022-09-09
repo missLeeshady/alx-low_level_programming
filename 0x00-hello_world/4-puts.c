3b38e84436ccb5669bdbb0f236b3ded8ffaf7d37
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-09 03:55:53 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - this is the starting point of the program
-*/
-void main(void)
+ * main - is the starting point of the program
+ *
+ * return value of main(): shows how the program is exited
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
