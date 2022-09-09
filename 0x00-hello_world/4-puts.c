7a66167040ddb1ff6a8c73b3f61f2ca9cd0e09a2
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-08 17:00:15 -0700

    please work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Returns: Always 0 (Success)
  */
-int main()/* void this specifies there are no arguments taken by main */
+int main(void)/* description of main return value; the return value is zero hence, void */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
