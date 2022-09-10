96b2454f15c9cfd86db79ca847155991a510a2ae
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-10 09:26:04 -0700

    print single base 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,14 @@
-iiiiiiiii
+#include <stdio.h>
+
+/* main - entry point */
+/* return: always 0 (success) */
+
+int main()
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
