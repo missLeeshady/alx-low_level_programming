0e3c7c819f8436834e85dad378c941013d110542
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-10 10:59:35 -0700

    print alphabets reversed in lowercase

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
-iiiii
+#include <stdio.h>
+
+/*main - entry point */
+/* return: always (0)*/
+
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
