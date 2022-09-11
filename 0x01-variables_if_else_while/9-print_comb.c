4a3ea6e271ce83e189bc0b91daa995e97d5b2284
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-11 12:45:38 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,15 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-		putchar(i);
-	putchar(',');
+		{
+			putchar(i);
+			if (i == '9')
+			{
+			continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
 
 	return (0);
