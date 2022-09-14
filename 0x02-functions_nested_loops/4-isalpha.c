d3d413769b57f8e9730ac18d16f5ab0e1b1d580b
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-13 17:37:44 -0700

    function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main function - entry point
+*
+* return type: int 
+*
+* function _isalpha:  checks for alphabetic character.
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - entry point 
+*
+* Return: always int,c or 0.
+*
+*/
+int _isalpha(int c);
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
