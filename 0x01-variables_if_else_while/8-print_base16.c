b18830306b1d64520cf67e2d3d3f08af202622ef
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-17 12:07:35 -0700

    correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/* main - entry point */
-/* return: always 0 (success) */
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
 
 int main(void)
 {
 	int i;
-	
+
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
 	for (i = 'a'; i <= 'f'; i++)
