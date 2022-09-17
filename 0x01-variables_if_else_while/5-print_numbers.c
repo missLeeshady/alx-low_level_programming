5cffe6e94145df24a9c681de91d35148d9903f79
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-17 11:52:21 -0700

    correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/* main - entry point */
-/* return: always 0 (success) */
+/**
+* main - program entry point
+* void: return type
+* Return: always 0 (success)
+*/
 
-int main()
+int main(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	printf("%d", i);
 	printf("\n");
