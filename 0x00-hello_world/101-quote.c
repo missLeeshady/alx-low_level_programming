1c94003aed8332db2c1a3c9032d6eae11ec850a9
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-17 10:12:55 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - entry point
-*
-* return - return value is void; 0.
+* main - the entry point
+* void: the return value
+* Return: always 0
 */
 
-int main()
+int main(void)
 {
-
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 77);
 	 
+	return (1);
 
-	return (0);
 }
