5cfd1d6b40f2dd1f888628f2fbbf70e87542028d
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-20 15:42:19 -0700

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_rev - prints a string in reverse,
 * followed by '\n'.
 * @s: is the string to be printed
-* Return: Nthing
+* Return: Nothing
 */
 void print_rev(char *s)
 {
 	if (*s != '\0')
-	
 	{
 		print_rev(s + 1);
-		_putchar(*s);
+		printf("%c", *s);
 	}
 }
