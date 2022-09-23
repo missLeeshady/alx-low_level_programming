783959932d05938ac2ed2aab9bc9a9fe24f66f85
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-23 12:22:28 -0700

    correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-#define MAX 100
 
 /**
 * cap_string - capitalizes all the words n a string
 * @str: string that will be capitalized
 * Return: string
 */
-int main(void)
+char *cap_string(char *str)
 {
-	char str[MAX] = {0};	
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if(i==0)
+	if (i == 0)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z'))
-		{	
-			str[i] = str[i] - 32;
-			continue;
-		}
-		if (str[i] == ' ')
-		{
-			++i;
-		}
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-			continue;
-		}
-		else
-		{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+	if ((str[i] >= 'a' && str[i] <= 'z'))
+	{
+		str[i] = str[i] - 32;
+		continue;
 	}
-
-return (0);
+	if (str[i] == ' ')
+	{
+		++i;
+	}
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+		continue;
+	}
+	else
+	{
+	if (str[i] >= 'A' && str[i] <= 'Z')
+	{
+		str[i] = str[i] + 32;
+	}
+	}
+	}
+}
+	return (str);
 }
