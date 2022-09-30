7ee1040a24c3535c0c5b179f5871b269862e2038
missLeeshady <beadsbyalfiyya@gmail.com>
2022-09-30 15:22:18 -0700

    a program that prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 
 /**
 * main - entry point of arguments
-* @c: counter of argument
-* @v: an array of size argc
+* @argc: counter of argument
+* @argv: an array of size argc
 * Return: always integer
 */
 int main(int argc, char *argv[])
 {
-	while (argc --)
+	while (argc--)
 	printf("%s\n", *argv++);
 	return (0);
 }
