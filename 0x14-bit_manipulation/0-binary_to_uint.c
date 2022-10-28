4646c49e663125a6494b9fc5c91bd3613fd322bb
missLeeshady <beadsbyalfiyya@gmail.com>
2022-10-28 07:13:54 -0700

    function that prints the binary representation of a number.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* binary_to_unit - converts a binary number to an unsigned int
+* binary_to_uint - converts a binary number to an unsigned int
 * @b: pointer to a string of 0 and 1 chars
 * Return: the converted number, or 0 if there is
 * there is one or more chars in the string b that is not 0 or 1
